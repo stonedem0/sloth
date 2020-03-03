6196f44284cd8467f2766abae6ad813405888c9f
asya <anundam@gmail.com>
2020-03-03 23:00:54 +0000

    add count for urls, add flag and err handling

--- sloth.go
+++ sloth.go
@@ -11,41 +11,50 @@ import (
 )
 
 func main() {
+	count := flag.Int("count", 10, "number of requests")
 	flag.Parse()
 	urls := flag.Args()
+
 	results := make(chan Result)
-	go Sloth(urls, results)
-	for a := 0; a < len(urls); a++ {
+	go Sloth(urls, *count, results)
+	for a := 0; a < len(urls)**count; a++ {
 		r := <-results
-		fmt.Println(aurora.Yellow("Response from: "), aurora.Green(r.URL), aurora.Yellow("took: "), aurora.Magenta(r.Duration))
+
+		if r.Error == nil {
+			fmt.Printf("Response %d from %s took %s \n", aurora.Yellow(a), aurora.Green(r.URL), aurora.Magenta(r.Duration))
+			continue
+		}
+		fmt.Printf("Response %d from %s has an error: %s \n", a, r.URL, r.Error)
 	}
 	close(results)
 }
 
 type Result struct {
 	Error    error
+	Index    int
 	Duration time.Duration
 	URL      string
 }
 
 // Sloth ...
-func Sloth(urls []string, res chan Result) {
+func Sloth(urls []string, count int, res chan Result) {
 	var wg sync.WaitGroup
-	wg.Add(len(urls))
+	wg.Add(len(urls) * count)
 	for _, val := range urls {
-		go func(val string) {
-			defer wg.Done()
-			start := time.Now()
-
-			r, err := http.Get(val)
-			if err != nil {
-				res <- Result{URL: val, Error: err}
-				return
-			}
-			defer r.Body.Close()
-			elapsed := time.Since(start).Round(time.Millisecond)
-			res <- Result{Duration: elapsed, URL: val}
-		}(val)
+		for a := 0; a < count; a++ {
+			go func(val string) {
+				defer wg.Done()
+				start := time.Now()
+				r, err := http.Get(val)
+				if err != nil {
+					res <- Result{URL: val, Error: err}
+					return
+				}
+				defer r.Body.Close()
+				elapsed := time.Since(start).Round(time.Millisecond)
+				res <- Result{Duration: elapsed, URL: val}
+			}(val)
+		}
 	}
 
 	wg.Wait()
