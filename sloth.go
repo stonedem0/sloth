aebd298e556ecaa920004ac0e220f25e4133cd50
asya <anundam@gmail.com>
2020-03-04 13:59:34 +0000

    add average respons speed check

--- sloth.go
+++ sloth.go
@@ -14,19 +14,24 @@ func main() {
 	count := flag.Int("count", 10, "number of requests")
 	flag.Parse()
 	urls := flag.Args()
-
 	results := make(chan Result)
+	tmp := make([]time.Duration, len(urls)**count)
 	go Sloth(urls, *count, results)
 	for a := 0; a < len(urls)**count; a++ {
 		r := <-results
-
+		tmp = append(tmp, r.Duration)
 		if r.Error == nil {
-			fmt.Printf("Response %d from %s took %s \n", aurora.Yellow(a), aurora.Green(r.URL), aurora.Magenta(r.Duration))
+			fmt.Printf("Response %-5d from %s took %s\n", aurora.Yellow(r.Index), aurora.Green(r.URL), aurora.Magenta(r.Duration))
 			continue
 		}
-		fmt.Printf("Response %d from %s has an error: %s \n", a, r.URL, r.Error)
+		fmt.Printf("Response %d from %s has an error: %s\n", r.Index, r.URL, r.Error)
 	}
 	close(results)
+	var sum time.Duration
+	for _, t := range tmp {
+		sum = sum + t
+	}
+	fmt.Printf("Average respond speed: %s\n", aurora.Magenta(sum/time.Duration(*count)))
 }
 
 type Result struct {
@@ -42,20 +47,19 @@ func Sloth(urls []string, count int, res chan Result) {
 	wg.Add(len(urls) * count)
 	for _, val := range urls {
 		for a := 0; a < count; a++ {
-			go func(val string) {
+			go func(val string, index int) {
 				defer wg.Done()
 				start := time.Now()
 				r, err := http.Get(val)
 				if err != nil {
-					res <- Result{URL: val, Error: err}
+					res <- Result{Index: index, URL: val, Error: err}
 					return
 				}
 				defer r.Body.Close()
 				elapsed := time.Since(start).Round(time.Millisecond)
-				res <- Result{Duration: elapsed, URL: val}
-			}(val)
+				res <- Result{Index: index, Duration: elapsed, URL: val}
+			}(val, a)
 		}
 	}
-
 	wg.Wait()
 }
