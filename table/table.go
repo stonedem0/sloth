83ba4522e03047031e24c43adfa3b7ca27cf70ab
asya <anundam@gmail.com>
2020-04-23 12:19:16 +0100

    clean up

--- table/table.go
+++ table/table.go
@@ -35,12 +35,19 @@ func calculateResults(t []time.Duration, c int, url string) []string {
 
 // Table is a struct which contains all columns and rows for table
 type Table struct {
-	columns  [][]string
-	rows     string
-	length   int
-	row      int
-	colSizes []int
-	data     [][]string
+	columns    [][]string
+	rows       string
+	length     int
+	row        int
+	colSizes   []int
+	data       [][]string
+	bg         uint8
+	text       uint8
+	padding    int
+	headers    []string
+	header     string
+	headerBg   uint8
+	headerText uint8
 }
 
 func (t *Table) saveColSizes(columns [][]string) []int {
@@ -55,34 +62,44 @@ func (t *Table) saveColSizes(columns [][]string) []int {
 }
 
 //Adding row to a table
-func (t *Table) addRow(data [][]string, textColor uint8, bgColor uint8, padding int) {
-	p := " "
-	rows := ""
-	for _, c := range data {
-		rows += aurora.Index(textColor, strings.Repeat(pad, padding)).BgIndex(bgColor).String()
-		for i, co := range c {
-			p = aurora.Index(textColor, strings.Repeat(pad, (t.colSizes[i]+padding)-len(co))).BgIndex(bgColor).String()
-			rows += aurora.Index(textColor, co).BgIndex(bgColor).String() + p
-		}
-		rows += "\n"
-	}
-	t.rows += rows
+func (t *Table) addRow(columns []string, textColor uint8, bgColor uint8, padding int) {
+	t.data = append(t.data, columns)
+	t.bg = bgColor
+	t.text = textColor
+	t.padding = padding
+	t.colSizes = make([]int, len(columns))
+	t.saveColSizes(t.data)
 
 }
 
 //Adding header
 func (t *Table) addHeader(headers []string, textColor uint8, bgColor uint8, padding int) {
+	t.headers = append(headers)
+	t.headerBg = bgColor
+	t.headerText = textColor
+	// p := " "
+	// rows := ""
+	// rows += aurora.Index(textColor, strings.Repeat(pad, padding)).BgIndex(bgColor).String()
+	// for i, co := range headers {
+	// 	p = aurora.Index(textColor, strings.Repeat(pad, (t.colSizes[i]+padding)-len(co))).BgIndex(bgColor).String()
+	// 	rows += aurora.Index(textColor, co).BgIndex(bgColor).Bold().String() + p
+
+	// }
+	// rows += "\n"
+	// t.rows += rows
+
+}
+
+func (t *Table) printHeader(headers []string, textColor uint8, bgColor uint8, padding int) {
+	header := ""
 	p := " "
-	rows := ""
-	rows += aurora.Index(textColor, strings.Repeat(pad, padding)).BgIndex(bgColor).String()
+	header += aurora.Index(t.text, strings.Repeat(pad, t.padding)).BgIndex(t.headerBg).String()
 	for i, co := range headers {
-		p = aurora.Index(textColor, strings.Repeat(pad, (t.colSizes[i]+padding)-len(co))).BgIndex(bgColor).String()
-		rows += aurora.Index(textColor, co).BgIndex(bgColor).String() + p
+		p = aurora.Index(t.headerText, strings.Repeat(pad, (t.colSizes[i]+padding)-len(co))).BgIndex(t.headerBg).String()
+		header += aurora.Index(t.headerText, co).BgIndex(t.headerBg).Bold().String() + p
 
 	}
-	rows += "\n"
-	t.rows += rows
-
+	t.header += header
 }
 
 func (t *Table) addStars() {
@@ -92,35 +109,40 @@ func (t *Table) addStars() {
 
 //Printing all rows
 func (t *Table) printTable() {
+	p := " "
+	rows := ""
+	t.printHeader(t.headers, t.headerBg, t.headerText, t.padding)
+
+	for _, c := range t.data {
+		rows += aurora.Index(t.text, strings.Repeat(pad, t.padding)).BgIndex(t.bg).String()
+		for i, co := range c {
+			p = aurora.Index(t.text, strings.Repeat(pad, (t.colSizes[i]+t.padding)-len(co))).BgIndex(t.bg).String()
+			rows += aurora.Index(t.text, co).BgIndex(t.bg).String() + p
+		}
+		rows += "\n"
+	}
+	t.rows += rows
+	fmt.Printf("%+2v\n", t.header)
 	fmt.Printf("%+2v\n", t.rows)
 }
 
 // PrintTable accepts map of results and print a table with it.
 func PrintTable(m map[string][]time.Duration, c int) {
 	t := Table{}
-	size := 0
-	for k, _ := range m {
-		if len(k) > 0 {
-			size++
-		}
-	}
-	t.colSizes = make([]int, size)
-	// results := []string{}
 	data := [][]string{}
 	headers := []string{"URL", "average", "min", "max"}
+	// test := []string{"meh", "bleh", "dude", "123456789019293"}
+	t.addHeader(headers, softPink, purple, 3)
 
 	for k, v := range m {
 		runes := []rune(k)
 		prettyURL := string(runes[8:])
-		// results = calculateResults(v, c, prettyURL)
+		t.addRow(calculateResults(v, c, prettyURL), purple, softPink, 3)
 		data = append(data, calculateResults(v, c, prettyURL))
-		// _ = results
 
 	}
 
-	t.saveColSizes(data)
-	t.addHeader(headers, softPink, purple, 3)
-	t.addRow(data, purple, softPink, 3)
+	// t.addRow(test, purple, softPink, 3)
 	t.printTable()
 
 }
